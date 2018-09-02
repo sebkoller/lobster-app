a943839dceac71cffb9df51f21b6301d398a2a3a
Nikhil Jha <hi@nikhiljha.com>
2018-09-02 12:59:38 -0700

    support fdroid

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,17 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+@import HockeySDK;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+    [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"5487663be1884d1a96f2b8302a0124ab"];
+    // Do some additional configuration if needed here
+    [[BITHockeyManager sharedHockeyManager] startManager];
+    [[BITHockeyManager sharedHockeyManager].authenticator
+     authenticateInstallation];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
